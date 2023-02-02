4bc654af91717f6e2df1f5907341ad2ba73480db
grem-newfield <novoger123@gmail.com>
2023-02-02 03:19:38 +0200

    u

--- banana/shared/fonts.nix
+++ banana/shared/fonts.nix
@@ -7,7 +7,7 @@
   fonts = {
     fonts = [ 
       (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }) 
-      (pkgs.iosevka.comfy) 
+      (pkgs.iosevka-comfy.comfy) 
     ] ++ lib.attrValues { inherit (pkgs) ubuntu_font_family; };
   fontconfig.enable = true;
   };
