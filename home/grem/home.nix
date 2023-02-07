de57228dd3f5078faa9dfbafc9dad0c5e91b915e
grem-newfield <novoger123@gmail.com>
2023-02-07 02:04:50 +0200

    u

--- home/grem/home.nix
+++ home/grem/home.nix
@@ -15,6 +15,10 @@
 
     # copium copying
     file."/home/grem/.config/awesome".source = ./awesome;
+    file."/home/grem/.config/ames".source = fetchTarball {
+      url = "https://github.com/eshrh/ames/archive/refs/tags/v0.1.5.tar.gz";
+      sha256 = "0iyic1g9b6r2kp38ly1iy3qar660qg5k54zycr2g5d4aanac36yc";
+    };
     file."/home/grem/.config/handlr/mimeapps.list".source = ./mimeapps.list;
   };
 
